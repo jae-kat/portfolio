18cf1ec8b7a39338a73285dac508c87984c75e35
katharina jäger <jaeger.kt@gmail.com>
2022-01-10 14:29:20 +0100

    Add info about me, add contact info

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -18,12 +18,11 @@ function Contact() {
               Contact
             </h1>
             <p className="description">
-              Incididunt in cillum magna occaecat nisi qui in exercitation
-              labore anim adipisicing amet irure tempor.
+              You can find me on twitter @jae_katharina
             </p>
             <ul className="contact-links">
-              <li className="contact-item">Email: lutfhi@me.com</li>
-              <li className="contact-item">Phone: +6212344321007</li>
+              <li className="contact-item">Email: @gmail.com</li>
+              <li className="contact-item">Phone: +43</li>
             </ul>
           </div>
         </div>
